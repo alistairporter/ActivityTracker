410aee0eabb687745d4b15a4a96dff786d42e221
michaellin2 <80468816+michaellin2@users.noreply.github.com>
2021-03-31 09:18:29 +0100

    Add files via upload

---
+++ web/homepage.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+	<link rel="stylesheet" href="css/css.css">
+	<script src="js/js.js"></script>
+</head>
+
+<body>
+	
+    <div class="sign-in-page">
+		<a href="homepage.html"><img src="images/logo.png" alt=""></a>
+        <div class="user-icon">
+            <div class="dropbtn"><img src="images/user-icon.png" alt="user-icon"> 
+                <i class="drop-down"></i>
+            </div>
+              <div class="dropdown-content">
+                <a href="profile.html">My profile</a>
+                <a href="activity.html">Activities</a>
+                <a href="index.html">Logout</a>
+              </div>
+        </div>
+	</div>	
+
+    <div class="navbar">
+        <a href="homepage.html">Dashboard</a>
+        <a href="log.html">Log</a>
+    </div>
+
+    <p>Date</p>
+    <div class="data-analyse-box">
+        <p>Report(Daily/Weekly)</p>
+        <button type="button">Button1</button>
+        <button type="button">Button2</button>
+        <div class="data-box">
+
+        </div>
+    </div>
+
+    <div class="personal-status-box">
+        <p>Personal Status</p>
+        <button type="button">Button1</button>
+        <button type="button">Button2</button>
+        <div class="data-box">
+
+        </div>
+    </div>
+
+    <div class="hydrogen-box">
+        <p>Hydrogen situation</p>
+        <button type="button">Button1</button>
+        <button type="button">Button2</button>
+        <div class="data-box">
+
+        </div>
+    </div>
+
+    <div class="extra-box">
+        <div class="calories-burnt-box">
+            <p>Calorie burnt</p>
+            <p1>Daily goal</p1>
+        </div>
+
+        <div class="activity-box">
+            <a href="activity.html"><button type="activity-button">View activitie</button></a>
+        </div>
+    </div>
+
+    <div class="content-box">
+        
+    </div>
+</body>
+   
+
+</html>
