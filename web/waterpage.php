cc964db4729b1884fa0da38518f801aa1cdc2b2e
michaellin2 <80468816+michaellin2@users.noreply.github.com>
2021-04-05 12:49:30 +0100

    Update waterpage.php

--- web/waterpage.php
+++ web/waterpage.php
@@ -19,39 +19,22 @@
 		<p>Water</p>
 		<div class="water-goal-box">
 			<p>Goal</p>
-			<select>
-				<option value = 500ml>500ml</option>>
-				<option value = 1L>1L</option>
-				<option value = 1.5L>1.5L</option>
-				<option value = 2L>2L</option>
-				<option value = 2.5L>2.5L</option>
-				<option value = 3L>3L</option>
-				<option value = 3.5L>3.5L</option>
-				<option value = 4L>4L</option>
-			</select>
+			<input type="text" placeholder="Liter">
+			<p>Water</p>
+			<input type="text" placeholder="Liter">
 		</div>
 	</div>
 
-	<div class="add-water-box">
+	<div class="water-box">
 		<div class="add-water">
 			<p>Add water </p>
-			<select>
-				<option value = 500ml>500ml</option>>
-				<option value = 1L>1L</option>
-				<option value = 1.5L>1.5L</option>
-				<option value = 2L>2L</option>
-				<option value = 2.5L>2.5L</option>
-				<option value = 3L>3L</option>
-				<option value = 3.5L>3.5L</option>
-				<option value = 4L>4L</option>
-			</select>
+			<input type="text" placeholder="Liter">
 		</div>
-	</div>
+	
 
-	<div class="goal-water-box">
-		<div class="goal-water">
+		<div class="goal-water-box">
 			<p>Goal water</p>
-			<input type="text" placeholder="">
+			<input type="text" placeholder="Liter">
 		</div>
 
 		<div class="water-update-button">
