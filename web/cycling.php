0d633900e2fbd41a582a17b2ed39e570ef202a6c
Alistair Porter <alistair_porter@outlook.com>
2021-04-09 13:22:52 +0100

    initial activity recording for cycling running walking.

--- web/cycling.php
+++ web/cycling.php
@@ -5,12 +5,59 @@
 <head>
 	<meta charset="utf-8"> 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Cycling</title>
+	<title>Running</title>
 	<link rel="stylesheet" href="css/css.css">
 	<script src="js/js.js"></script>
 </head>
 
 <body>
+    <?php
+		include "includes/database.php"; 
+		$durationMindb = '';
+		$burntCaloriesdb = '';
+		$typedb = '';
+
+		// now read from database select id by given username 
+		$userid =$_SESSION["userid"];
+
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		$durationMin= $_POST["durationMin"];
+		$burntCalories = $durationMin * 9.5;
+		$type="cycling";
+		
+	// insert values in the data base and userid 
+		$sql = "INSERT INTO activity(durationMin, burntCalories, userid, type) VALUE ($durationMin, $burntCalories, $userid,'$type') ";
+
+		if (mysqli_query($conn, $sql)) { 
+	        //continue show the rest of html
+	 		// get the las id
+	 		$lastId = $conn->insert_id;
+	 		header("Location: /cycling.php?id=".$lastId);
+		}
+	
+		else {
+	         echo "Error: " . $sql . "<br>" .
+	         mysqli_error($conn); 
+	        }
+	    }
+	        elseif (isset($_GET['id'])) {
+				$id = $_GET['id'];
+				//read from data base
+				$sql = "SELECT durationMin, burntCalories FROM activity where id=$id and userid = $userid ";
+				$result = $conn->query($sql);
+				//result array
+		        $row = $result->fetch_assoc() ;
+	        	if (!$result) {
+	            die('Could not query:' . mysql_error());
+	        } 
+	        $durationMindb = $row['durationMin'];
+			$burntCaloriesdb = $row['burntCalories'];
+		}
+
+
+
+	?>
+
     <div class="sign-in-page">  
         <a href="homepage.php"><img src="images/logo.png" alt=""></a>
     </div>
@@ -18,11 +65,28 @@
     <div class="cycling-container">
         <h1>GPS</h1>
     </div>
-
+    
     <div class="cycling-button-container">
-        <button type="button" value="start button">Start</button>
-        <button type="button" value="end button">End</button>
+        <button type="button" value="Start GPS Record">Start GPS Record</button>
+        <button type="button" value="Stop GPS Record">Stop GPS Record</button>
     </div>
+    <form action="#" method="post">
+    <div class="cycling-duration-container">
+      <div class="duration">
+        <p>Duration: </p>
+        <input type="text" placeholder="" name="durationMin">
+        <p>minutes</p>
+      </div>
+      <div class="calories-burnt">
+        <p>Calorie burnt:</p>
+        <input type="text" placeholder="" value="<?php echo $burntCaloriesdb;?>">
+        <p>cal</p>
+      </div>
+      <div class="submit-button">
+        <imput type="submit" value="submit">
+      </div>
+    </div>
+    </form>
 </body>
 
 </html>
