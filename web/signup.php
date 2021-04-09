0e66de2487ea2053063573db245801f8ebeaefe7
michaellin2 <80468816+michaellin2@users.noreply.github.com>
2021-04-09 21:44:06 +0100

    Update signup.php

--- web/signup.php
+++ web/signup.php
@@ -46,7 +46,7 @@ session_start();
 
 
 	<div class="sign-in-page">
-        <a href="index.php"><img src="images/logo.png" alt=""></a>
+        <a href="index.php"><img src="images/logo1.png" alt=""></a>
     </div>
 
 	<div class="sign-up-form">
